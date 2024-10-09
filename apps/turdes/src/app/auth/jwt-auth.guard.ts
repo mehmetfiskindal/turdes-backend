bfd1c72f573d117fe8c37788ac247f24c1ba439f
mehmetfiskindal <mehmetfiskindal@gmail.com>
2024-10-09 13:26:34 +0300

    .

--- apps/turdes/src/app/auth/jwt-auth.guard.ts
+++ apps/turdes/src/app/auth/jwt-auth.guard.ts
@@ -4,25 +4,48 @@ import {
   ExecutionContext,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Request } from 'express';
+
 import { AuthGuard } from '@nestjs/passport';
 import { JwtService } from '@nestjs/jwt';
 import { Reflector } from '@nestjs/core';
-
+declare module 'express' {
+  export interface Request {
+    user?: { id: string; username: string; email: string }; // Replace with the actual user type
+  }
+}
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(
-    private jwtService: JwtService,
-    private reflector: Reflector,
-  ) {
+  constructor(private jwtService: JwtService, private reflector: Reflector) {
     super();
   }
 
-  canActivate(context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest();
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException('Token not found');
+    }
+
+    try {
+      const payload = this.jwtService.verify(token);
+      request.user = payload;
+    } catch (error: Error | unknown) {
+      console.error(error);
+      throw new UnauthorizedException('Invalid token');
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | null {
     const authHeader = request.headers.authorization;
     if (!authHeader) {
-      throw new UnauthorizedException('Authorization header missing');
+      return null;
     }
-    return super.canActivate(context);
+
+    const [type, token] = authHeader.split(' ');
+    return type === 'Bearer' ? token : null;
   }
 }
