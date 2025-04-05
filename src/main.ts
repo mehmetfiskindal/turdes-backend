2508fdfa355b19deb281f31761c3c06a2df4cf1e
mehmet fiskindal <developersailor@gmail.com>
2025-04-05 22:28:40 +0300

    fix = logger code remove

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
-
-import { Logger } from '@nestjs/common';
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import * as dotenv from 'dotenv';
@@ -17,7 +14,6 @@ async function bootstrap() {
   app.use(cookieParser());
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
 
   const config = new DocumentBuilder()
     .setTitle('Turdes API')
@@ -30,8 +26,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   await app.listen(3000);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-  );
+
 }
 bootstrap();
